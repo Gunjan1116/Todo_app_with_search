904a7df2b410b461cd2128bd9e54a109c6c98734
Gunjan1116 <kumargunjan1116@gmail.com>
2023-06-28 13:48:30 +0530

    added response status in user controller

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -9,21 +9,21 @@ const register=async(req,res)=>{
     try {
         let reqData=await Usermodel.find({email});
         if(reqData.length>0){
-            return res.json({"msg":"You are already register"})
+            return res.status(400).json({"msg":"You are already register"})
         }
         bcrypt.hash(password,5,async(err,hash)=>{
             if(err){
                 console.log("error from hashing password",err);
-                res.json({"msg":"error from hashing password"})
+                res.status(500).json({"msg":"error from hashing password"})
             }else{
                 let registerData=new Usermodel({name,email,password:hash});
                 await registerData.save();
-                res.json({"msg":"Successfully register"})
+                res.status(201).json({"msg":"Successfully register"})
             }
         })
     } catch (error) {
         console.log("error from register route",error);
-        res.json({"msg":"error in register a user"})
+        res.status(500).json({"msg":"error in register a user"})
     }
 }
 
@@ -32,21 +32,21 @@ const login=async(req,res)=>{
     try {
         let reqData=await Usermodel.find({email});
         if(reqData.length==0){
-            return res.json({"msg":"register first"})
+            return res.status(400).json({"msg":"register first"})
         }else{
             bcrypt.compare(password,reqData[0].password,async(err,result)=>{
                 if(result){
                     let token=jwt.sign({userId:reqData[0]._id,email:reqData[0].email},process.env.Key);
-                    res.json({"msg":"Login Success","token":token,"name":reqData[0].name})
+                    res.status(200).json({"msg":"Login Success","token":token,"name":reqData[0].name})
                 }else{
-                    res.json({"msg":"Wrong Credentials"})
+                    res.status(401).json({"msg":"Wrong Credentials"})
                 }
             })
         }
 
     } catch (error) {
         console.log("error from login route",error);
-        res.json({"msg":"error in login a user"})
+        res.status(500).json({"msg":"error in login a user"})
     }
 }
 
